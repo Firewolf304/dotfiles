e9d56bb75ef3d8c0314530752594b75b839fa6d4
Firewolf304 <firewolf@glitchdos.localdomain>
2026-08-17 01:10:42 +0700

    Tempory rm Linux/ansible + add wsl/fedora42 zshrc

--- Global/wezterm/wezterm.lua
+++ Global/wezterm/wezterm.lua
@@ -10,7 +10,7 @@ function scheme_for_appearance(appearance)
 	end
 end
 config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
-
+-- config.font = wezterm.font("JetBrains Mono Nerd")
 if is_macos or true then
 	config.keys = {
 		-- Option+Up
